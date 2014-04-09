d7164ebda273321a8b856521f0d20e06c38f1131
wendycan <wilxnwang@gmail.com>
2014-04-09 11:59:57 +0800

    Remove debug files

--- Debug/config.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-config.obj: ../config.c
-config.obj: ../config.h
-config.obj: D:/ccsv5/tools/compiler/msp430/include/stdint.h
-
-../config.c: 
-../config.h: 
-D:/ccsv5/tools/compiler/msp430/include/stdint.h: 
